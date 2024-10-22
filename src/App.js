35eed3d25477825dfb9b728287f4deadf7899e9f
Alex040822 <00098818@uca.edu.sv>
2024-10-22 17:15:55 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='principal'>
+        <div className='firtsSec'>
+          <h1>Mortgage Calculator</h1>
+          <button className='btn-clear'>Clear All </button>
+        </div>
+
+        <form className='form'> 
+          <label for='amount' className='label-amount'> Mortgage Amount </label>
+          <div className='secodSec'>
+            <span className='symbol'>€</span>
+            <input type='number' id='amount' className='input-amount' required/>
+          </div>
+
+          <div className='col-two'>
+            <div className='form-c'>
+              <div className='inputs-groups'>
+                <label for='years' className='years-label'>Introduce years</label>
+                <span className='symbol-years'>Years</span>
+                <input type='number' id='years' className='input-years' required/>
+              </div>
+
+
+              <div className='form-c'>
+                <label for='interest' className='interest-label'>Introduce interest rate</label>
+                <span className='symbol-interest'>%</span>
+                <input type='number' id='interest' className='input-interest' step='0.01' required/>
+              </div>
+          
+          <fieldset className='mortgage-type'>
+            <label className='fiel-title-label'>Morgage Type</label>
+            <div className='type-group'>
+              <input required type='radio' name='type' id='repayment' value='repayment' />
+              <label for='repayment' className='repayment-label'>Repayment</label>
+            </div>
+
+            <div>
+            <div className='type-group'>
+            <input type='radio' name='type' id='interest' value='interest' required/>
+            <label for='interest' className='interest-label'>Interest Only</label>
+          </div>
+            </div>
+          </fieldset>
+
+          <button className='btn-calculator' type='submit'>
+            Calculate Repayments
+          </button>
+            </div>
+          </div>
+
+        </form>
+      
+      </div>
+
     </div>
   );
 }
